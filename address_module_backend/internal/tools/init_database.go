e8f4af25f1926ec28da22ef7e6124c5b3543fe05
Leon Lobert <leon.lobert@student.reutlingen-university.de>
2025-03-27 13:28:23 +0100

    Made name colloum in user table not unique

--- address_module_backend/internal/tools/init_database.go
+++ address_module_backend/internal/tools/init_database.go
@@ -116,7 +116,7 @@ func (db *MySQLDB) SetupUsersTable() error {
 	query := `
     CREATE TABLE IF NOT EXISTS users (
         id INT AUTO_INCREMENT PRIMARY KEY,
-        username VARCHAR(50) NOT NULL UNIQUE,
+        username VARCHAR(50) NOT NULL,
         email VARCHAR(100) NOT NULL UNIQUE,
         hashed_password VARCHAR(255) NOT NULL,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
